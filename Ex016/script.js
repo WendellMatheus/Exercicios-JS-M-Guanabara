00e07af37f3f403fd9062953575575d84e269469
Wendell Matheus <wendellmatheus77@gmail.com>
2020-10-03 00:12:29 -0300

    Alteração no backgorund e responsividade

--- Ex016/script.js
+++ Ex016/script.js
@@ -1,30 +1,37 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar, preencha todos os campos.'
-        //window.alert('[ERRO] Faltam dados')
+    let ini = document.getElementById("txti");
+    let fim = document.getElementById("txtf");
+    let passo = document.getElementById("txtp");
+    let res = document.getElementById("res");
+  
+    if (
+      ini.value.length === 0 ||
+      fim.value.length === 0 ||
+      passo.value.length === 0
+    ) {
+      res.innerHTML = "Impossível contar!";
+      //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = `Contando: <br> `
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
-            p = 1
+      res.innerHTML = "Contando: <br>";
+      let i = Number(ini.value);
+      let f = Number(fim.value);
+      let p = Number(passo.value);
+      if (p <= 0) {
+        window.alert("Passo inválido! Considerando PASSO 1");
+        p = 1;
+      }
+      if (i < f) {
+        // Contagem crescente
+        for (let c = i; c <= f; c += p) {
+          res.innerHTML += ` ${c} \u{1F449}`;
         }
-        if (i < f) {
-            for(let ctd = i; ctd <= f; ctd += p) {
-            res.innerHTML += ` ${ctd} \u{1F60E}`
-            }
-        } else {
-            for (let ctd = i; ctd >= f; ctd -= p) {
-                res.innerHTML += ` ${ctd} \u{1F60E}`
-            }
+      } else {
+        //Contagem regressiva
+        for (let c = i; c >= f; c -= p) {
+          res.innerHTML += ` ${c} \u{1F449}`;
         }
-        res.innerHTML += `\u{1F3C1}`
+      }
+      res.innerHTML += `\u{1F3C1}`;
     }
-}
+  }
+  
